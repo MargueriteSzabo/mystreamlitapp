90a847e3d7b6221a3d8227edd17bf1cc785d46ae
Marguerite Szabo <szabo.marguerite@free.fr>
2023-01-28 10:00:25 +0100

    first push

---
+++ pages/page_01.py
@@ -0,0 +1,22 @@
+import requests
+import streamlit as st
+import numpy as np
+
+st.set_page_config(page_title="Hello", page_icon="")
+
+st.markdown('# Streamlit is awesome ! ')
+
+if st.button('click ;)'):
+    st.image('raw_data/le_wagon.png')
+
+query = st.text_input('Search for GIF')
+url = "https://api.giphy.com/v1/gifs/search"
+params={"api_key": st.secrets.api_key, "q": query, "limit":10}
+response = requests.get(url=url, params=params).json()
+
+while not query:
+    st.stop()
+
+gif_url = response['data'][np.random.randint(0,10)]['embed_url']
+st.write(f'<iframe src="{gif_url}" width="480" height="240">',
+         unsafe_allow_html=True,)
